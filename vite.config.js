ca5d1dd3890308b3c6786c65030847e6af02a793
miguel <josm_reyes@unicah.edu>
2023-11-02 00:06:37 -0600

    deploy second try

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,23 @@ import path, { resolve } from 'node:path'
 import { ViteMinifyPlugin} from 'vite-plugin-minify'
 import htmlPurge from 'vite-plugin-purgecss'
 export default defineConfig({
+    base: "/updated_portfolio-pw2/",
+    appType: 'mpa',
     build: {
-        
-    }, 
+        rollupOptions: {
+            input: Object.fromEntries(
+                [...glob.sync('./!(dist)/*.html').map(file => [
+                    file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                ]),
+                    ...glob.sync('./*.html').map(file => [
+                        file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                    ])]
+            ),
+        },
+    },
     plugins: [
         htmlPurge({}),
         ViteMinifyPlugin({})
-    ]
+    ],
     
-    base: "/updated_portfolio-pw2/"
 })
