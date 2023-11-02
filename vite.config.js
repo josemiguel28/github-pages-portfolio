970a2396724494f31f7aac8d83e01540584dc23d
miguel <josm_reyes@unicah.edu>
2023-11-01 23:27:53 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import * as glob from 'glob';
+import path, { resolve } from 'node:path'
+import { ViteMinifyPlugin} from 'vite-plugin-minify'
+import htmlPurge from 'vite-plugin-purgecss'
+export default defineConfig({
+    build: {
+        
+    }, 
+    plugins: [
+        htmlPurge({}),
+        ViteMinifyPlugin({})
+    ]
+})
